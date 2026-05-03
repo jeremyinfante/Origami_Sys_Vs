081315307698e46e62c4056c6cd7e293abcd9047
jere <jeremyjonasinfantemonegro@gmail.com>
2026-05-03 08:28:03 -0400

    mejorar index de materias primas

--- Origami Sys/Pages/MateriaPrima/Index.cshtml.cs
+++ Origami Sys/Pages/MateriaPrima/Index.cshtml.cs
@@ -12,6 +12,7 @@ namespace Origami_Sys.Pages.MateriaPrima
         public IndexModel(ApplicationDBContext context) => _context = context;
 
         public IList<Origami_Sys.Modelos.MateriaPrima> MateriasPrimas { get; set; }
+        public IList<string> Unidades { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -21,7 +22,38 @@ namespace Origami_Sys.Pages.MateriaPrima
             MateriasPrimas = await _context.MateriaPrima
                 .OrderBy(m => m.Nombre)
                 .ToListAsync();
+
+            Unidades = MateriasPrimas
+                .Select(m => m.UnidadMedida)
+                .Distinct()
+                .OrderBy(u => u)
+                .ToList();
+
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostEliminarAsync(int id)
+        {
+            var mp = await _context.MateriaPrima.FindAsync(id);
+            if (mp != null)
+            {
+                _context.MateriaPrima.Remove(mp);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostBorrarSeleccionAsync(List<int> ids)
+        {
+            if (ids != null && ids.Any())
+            {
+                var items = await _context.MateriaPrima
+                    .Where(m => ids.Contains(m.Id))
+                    .ToListAsync();
+                _context.MateriaPrima.RemoveRange(items);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
         }
     }
 }
