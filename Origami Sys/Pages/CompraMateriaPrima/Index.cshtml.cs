f5d731e6e03800a6ba117f4b3e92d9362c01adcb
jere <jeremyjonasinfantemonegro@gmail.com>
2026-05-03 09:19:58 -0400

    compra terminada.

--- Origami Sys/Pages/CompraMateriaPrima/Index.cshtml.cs
+++ Origami Sys/Pages/CompraMateriaPrima/Index.cshtml.cs
@@ -24,5 +24,32 @@ namespace Origami_Sys.Pages.CompraMateriaPrima
                 .ToListAsync();
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAnularAsync(int id)
+        {
+            var compra = await _context.CompraMateriaPrima
+                .Include(c => c.MateriaPrima)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (compra != null)
+            {
+                // Revertir stock
+                var mp = await _context.MateriaPrima.FindAsync(compra.MateriaPrimaId);
+                if (mp != null) mp.StockActual -= compra.Cantidad;
+
+                // Eliminar egreso asociado
+                var egreso = await _context.Transaccion
+                    .FirstOrDefaultAsync(t =>
+                        t.Tipo == Origami_Sys.Modelos.TipoTransaccion.Egreso &&
+                        t.Descripcion.Contains(compra.MateriaPrima.Nombre) &&
+                        t.Fecha.Date == compra.Fecha.Date &&
+                        t.Monto == compra.CostoTotal);
+                if (egreso != null) _context.Transaccion.Remove(egreso);
+
+                _context.CompraMateriaPrima.Remove(compra);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
     }
 }
